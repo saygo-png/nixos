642f2364808a15d9c7c70342532f47f85a2cc10f
saygo-png <saygo.mail@proton.me>
2024-06-11 22:47:07 +0200

    first commit

---
+++ configuration.nix
@@ -0,0 +1,258 @@
+#let
+#        constants = {
+#            username = "samsepi0l";
+#            home = "/home/samsepi0l";
+#            hostname = "nixos";
+#            flake-path = "...";
+#            system = "x86_64-linux";
+#            version = "release-24.05";
+#        };
+#    in {
+#    };
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    inputs.home-manager.nixosModules.default
+    #inputs.nixvim.nixosModules.nixvim
+  ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Warsaw";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = lib.mkForce "pl"; # needs to be forced
+    useXkbConfig = true; # use xkb.options in tty.
+  };
+
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+
+  # Define a user account. Don't forget to set a password with ‘passwd $USERNAME’.
+  users.users.samsepi0l = {
+    isNormalUser = true;
+    extraGroups = ["wheel" "networkmanager"]; # Enable ‘sudo’ for the user.
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+      # vim.
+    ];
+  };
+
+  ############
+  # Services #
+  ############
+
+  # Configure keymap in X11
+  services.xserver.xkb.layout = "pl";
+  services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable sound.
+  hardware.pulseaudio.enable = false;
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+  };
+
+  # Disable touchpad support (enabled default in most desktopManager).
+  services.libinput.enable = false;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    wget
+    git
+  ];
+
+  programs.zsh.enable = true;
+
+  ################
+  # HOME MANAGER #
+  ################
+
+  home-manager = {
+    extraSpecialArgs = {inherit inputs;};
+    backupFileExtension = "backup";
+    users.samsepi0l = {
+      home = {
+        username = "samsepi0l";
+        homeDirectory = "/home/samsepi0l";
+        stateVersion = "24.05"; # Dont change
+        packages = [
+          pkgs.hello
+          pkgs.htop
+          pkgs.alejandra
+          pkgs.rofi
+          pkgs.librewolf
+          pkgs.nil
+        ];
+        sessionVariables = {
+          # Default programs.
+          PAGER = "most";
+          # Systemd is retarded and doesnt use normal pager variable :DDDDD
+          SYSTEMD_PAGER = "most";
+          VISUAL = "nvim";
+          OPENER = "xdg-open";
+          EDITOR = "nvim";
+          TERMINAL = "alacritty";
+          TERMINAL_PROG = "alacritty";
+          BROWSER = "librewolf";
+          # Firefox hardware decode.
+          MOZ_X11_EGL = 1;
+          NO_AT_BRIDGE = 1;
+          # Unreal engine .net cli tool turn off telemetry.
+          DOTNET_CLI_TELEMETRY_OPTOUT = "true";
+        };
+      };
+
+      programs.home-manager.enable = true;
+      programs.git = {
+        enable = true;
+        package = pkgs.gitAndTools.gitFull;
+        userName = "saygo-png";
+        userEmail = "saygo.mail@proton.me";
+        aliases = {
+          undo = "reset HEAD~1 --mixed";
+          amend = "commit -a --amend";
+        };
+        extraConfig = {
+          color = {
+            ui = "auto";
+          };
+          diff = {
+            tool = "vimdiff";
+            mnemonicprefix = true;
+          };
+          merge = {
+            tool = "splice";
+          };
+          push = {
+            default = "simple";
+          };
+          pull = {
+            rebase = true;
+          };
+          core = {
+            excludesfile = "~/.gitignore_global";
+          };
+          branch = {
+            autosetupmerge = true;
+          };
+          rerere = {
+            enabled = true;
+          };
+        };
+      };
+      programs.vscode = {
+        enable = true;
+        package = pkgs.vscodium;
+        extensions = with pkgs.vscode-extensions; [
+          bbenoist.nix
+          kamadorueda.alejandra
+          jdinhlife.gruvbox
+          jnoortheen.nix-ide
+          vscodevim.vim
+        ];
+      };
+
+      programs.alacritty = {
+        enable = true;
+        # custom settings
+        settings = {
+          env.TERM = "xterm-256color";
+          font = {
+            size = 12;
+          };
+          scrolling.multiplier = 5;
+          selection.save_to_clipboard = true;
+        };
+      };
+      programs.zsh.enable = true;
+      programs.neovim = {
+        enable = true;
+        # extraconfig = builtins.readFile ./init.vim;
+      };
+      # programs.nixvim = {
+      #   enable = true;
+      #   colorschemes.gruvbox.enable = true;
+      # };
+    };
+  };
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "24.05"; # Did you read the comment?
+}
---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+
+    nixvim = {
+      url = "github:nix-community/nixvim/nixos-24.05";
+      # If using a stable channel you can use `url = "github:nix-community/nixvim/nixos-<version>"`
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-24.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: {
+    nixosConfigurations.default = nixpkgs.lib.nixosSystem {
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./configuration.nix
+        inputs.home-manager.nixosModules.default # useless?
+        #inputs.nixvim.homeManagerModules.nixvim
+        #inputs.nixvim.nixosModules.nixvim
+      ];
+    };
+  };
+}
