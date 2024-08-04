a95002787d49853a1adfd6890e77c2fb2343adb5
saygo-png <saygo.mail@proton.me>
2024-08-04 07:13:06 +0200

    fix light

--- hosts/thinkpad.nix
+++ hosts/thinkpad.nix
@@ -26,12 +26,12 @@
     enable = true;
     bindings = [
       {
-        keys = [224];
+        keys = [225];
         events = ["key"];
         command = "/run/current-system/sw/bin/light -A 10";
       }
       {
-        keys = [225];
+        keys = [224];
         events = ["key"];
         command = "/run/current-system/sw/bin/light -U 10";
       }
