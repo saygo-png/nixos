034acc40b60a836f0d5e7c1ede7b44172652d96d
saygo-png <saygo.mail@proton.me>
2024-09-23 04:50:28 +0200

    cmp is back

--- configuration.nix
+++ configuration.nix
@@ -2061,6 +2061,58 @@
             enable = true;
             settings.defaultFileExplorer = true;
           };
+
+          luasnip = {
+            enable = true;
+            extraConfig = {
+              enable_autosnippets = true;
+              store_selection_keys = "<Tab>";
+            };
+            fromVscode = [
+              {
+                lazyLoad = true;
+              }
+            ];
+          };
+
+          cmp = {
+            enable = true;
+            autoEnableSources = true;
+            settings = {
+              autocomplete = true;
+              performance = {
+                debounce = 200;
+                throttle = 200;
+                maxViewEntries = 5;
+                fetchingTimeout = 50;
+              };
+              snippet.expand = ''
+                function(args)
+                  require('luasnip').lsp_expand(args.body)
+                end
+              '';
+              sources = [
+                {name = "nvim_lsp";}
+              ];
+              mapping = {
+                "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+                "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+                "<C-j>" = "cmp.mapping.select_next_item()";
+                "<C-k>" = "cmp.mapping.select_prev_item()";
+                "<C-e>" = "cmp.mapping.abort()";
+                "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+                "<C-f>" = "cmp.mapping.scroll_docs(4)";
+                "<C-Space>" = "cmp.mapping.complete()";
+                "<CR>" = "cmp.mapping.confirm({ select = false })";
+                "<S-CR>" = "cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true })";
+              };
+
+              window = {
+                completion.scrollbar = true;
+                documentation.border = "single";
+              };
+            };
+          };
         };
 
         # }}}
