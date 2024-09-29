3f5213441df1abfa05577f2f84b0aea8f2b9e788
saygo-png <saygo.mail@proton.me>
2024-09-29 06:45:08 +0200

    convertlink (i wrote it in haskell btw)

--- configuration.nix
+++ configuration.nix
@@ -207,6 +207,66 @@
       text = builtins.readFile ./resources/scripts/format-udf.sh;
     })
 
+    (pkgs.writers.writeHaskellBin "convertlink" {
+        libraries = with pkgs; [
+        haskellPackages.directory_1_3_8_5
+        haskellPackages.unix_2_8_5_1
+        haskellPackages.process_1_6_20_0
+        haskellPackages.optparse-applicative
+        ];
+      }
+      ''
+        {-# LANGUAGE LambdaCase #-}
+        import Control.Monad (when)
+        import Options.Applicative
+        import System.Directory
+        import System.Posix.Files
+        import System.Process (callProcess)
+        import Text.Printf (printf)
+
+        data Sample = Sample
+          { path :: FilePath,
+            write :: Bool
+          }
+
+        sample :: Parser Sample
+        sample =
+          Sample
+            <$> argument
+              str
+              ( help "Target for the greeting"
+                  <> metavar "SYMLINK"
+                  <> help "Symlink that will be converted"
+              )
+            <*> switch
+              ( long "write"
+                  <> short 'w'
+                  <> help "Give write permissions to the resulting file"
+              )
+
+        main :: IO ()
+        main = convertlinkAndWrite =<< execParser opts
+          where
+            opts =
+              info
+                (sample <**> helper)
+                ( fullDesc
+                    <> progDesc "Convert a symlink to the file its pointing to"
+                )
+
+        convertlinkAndWrite :: Sample -> IO ()
+        convertlinkAndWrite (Sample filepath giveWrite) = do
+          pathIsSymbolicLink filepath >>= \case
+            True -> do
+              fileFromSymlink <- readSymbolicLink filepath
+              _ <- callProcess "cp" ["--remove-destination", fileFromSymlink, filepath]
+              printf "Converted symlink %s\n" filepath
+              when giveWrite $ do
+                _ <- callProcess "chmod" ["+w", filepath]
+                pure ()
+            False -> printf "convertlink: cannot convert %s: Not a symlink\n" filepath
+      '')
+
     (writeShellApplication {
       name = "update_mutable.sh";
       runtimeInputs = [coreutils];
