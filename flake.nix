ef32720dbe2f85acafad9305c7a3f99c13e67aec
saygo-png <saygo.mail@proton.me>
2024-09-29 05:14:30 +0200

    stylix stable

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
     stylix = {
-      url = "github:danth/stylix";
+      url = "github:danth/stylix/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.home-manager.follows = "home-manager";
     };
