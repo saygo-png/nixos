54b0f65987904d4d5d65c10876e8cebe961b7cdf
saygo-png <saygo.mail@proton.me>
2024-10-10 02:04:00 +0200

    feat: integrate AI for interactive commit message writing

--- modules/myAichat.nix
+++ modules/myAichat.nix
@@ -1,5 +1,4 @@
 {
-  config,
   conUsername,
   pkgs,
   ...
@@ -9,19 +8,18 @@
   ];
 
   home-manager.users.${conUsername} = {
-    xdg.configFile."aichat/config.yaml".text =
-      # yaml
-      ''
-        # model: openai:gpt-4o
-        clients:
-        - type: openai
-          # api_key comes from $OPENAI_API_KEY env var
-          # See: https://github.com/sigoden/aichat/wiki/Environment-Variables#client-related-envs
-          api_key: null
-        stream: true
-        # save: true
-        keybindings: vi
-      '';
+    # xdg.configFile."aichat/config.yaml".text =
+      # # yaml
+      # ''
+      #   model: gemini
+      #   clients:
+      #   - type: gemini
+      #     # api_key comes from $OPENAI_API_KEY env var
+      #     # See: https://github.com/sigoden/aichat/wiki/Environment-Variables#client-related-envs
+      #     api_key: null
+      #   # save: true
+      #   keybindings: vi
+      # '';
 
     xdg.configFile."aichat/roles.yaml".text =
       # yaml
