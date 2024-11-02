00a7acdfdf87fa787c71c89fd2ee1aa3e0313bd3
saygo-png <saygo.mail@proton.me>
2024-11-02 15:42:18 +0100

    feat: explicit theme options for qt5ct
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- modules/visuals/myKvantumBasedQT.nix
+++ modules/visuals/myKvantumBasedQT.nix
@@ -32,22 +32,22 @@
         #   force_raster_widgets = 1;
         #   ignored_applications = "@Invalid()";
         # };
-        #
-        # Interface = {
-        #   wheel_scroll_lines = 3;
-        #   menus_have_icons = true;
-        #   cursor_flash_time = 1200;
-        #   keyboard_scheme = 2; # X11
-        #   gui_effects = "@Invalid()";
-        #   stylesheets = "@Invalid()";
-        #   double_click_interval = 400;
-        #   underline_shortcut = 1; # ...
-        #   dialog_buttons_have_icons = 1; # ...
-        #   show_shortcuts_in_context_menus = true;
-        #   buttonbox_layout = 3; # GNOME dialog button layout
-        #   toolbutton_style = 4; # Follow the application style
-        #   activate_item_on_single_click = 1; # ... - i think that means let the application decide
-        # };
+
+        Interface = {
+          wheel_scroll_lines = 3;
+          menus_have_icons = true;
+          cursor_flash_time = 1200;
+          keyboard_scheme = 2; # X11
+          gui_effects = "@Invalid()";
+          stylesheets = "@Invalid()";
+          double_click_interval = 400;
+          underline_shortcut = 1; # ...
+          dialog_buttons_have_icons = 1; # ...
+          show_shortcuts_in_context_menus = true;
+          buttonbox_layout = 3; # GNOME dialog button layout
+          toolbutton_style = 4; # Follow the application style
+          activate_item_on_single_click = 1; # ... - i think that means let the application decide
+        };
       };
     in {
       ".config/qt5ct/qt5ct.conf".text = lib.generators.toINI {} (baseConfig
