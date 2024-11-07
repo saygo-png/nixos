563ecbb1cfddd1746732ae1e3836d394f90f5928
saygo-png <saygo.mail@proton.me>
2024-11-08 00:47:01 +0100

    refactor: No more devenv shells
    waiting for https://github.com/NixOS/nix/pull/6530
    
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- flake.nix
+++ flake.nix
@@ -91,46 +91,5 @@
         ./hosts/thinkpad/hardware-configuration-thinkpad.nix
       ];
     };
-
-    devShells = inputs.nixpkgs.lib.genAttrs ["x86_64-linux"] (system: {
-      default = import ./shell.nix {inherit system;};
-      # let
-      #   pkgs = inputs.nixpkgs.legacyPackages.${system};
-      # in
-      #   inputs.devenv.lib.mkShell {
-      #     inherit inputs pkgs;
-      #     modules = [
-      #       (_: {
-      #         # packages = [
-      #         #   pkgs.cargo
-      #         # ];
-      #
-      #         languages = {
-      #           nix.enable = true;
-      #           shell.enable = true;
-      #         };
-      #
-      #         pre-commit.hooks = {
-      #           commitlint-rs = {
-      #             enable = true;
-      #             entry = "commitlint --edit";
-      #             language = "rust";
-      #             package = pkgs-unstable.commitlint-rs;
-      #             pass_filenames = false;
-      #             fail_fast = true;
-      #             stages = ["commit-msg"];
-      #             extraPackages = [pkgs.cargo];
-      #           };
-      #           deadnix.enable = true;
-      #           shellcheck.enable = true;
-      #           # statix.enable = true;
-      #           treefmt.settings.formatters = [
-      #             pkgs.alejandra
-      #           ];
-      #         };
-      # })
-      # ];
-      # };
-    });
   };
 }
