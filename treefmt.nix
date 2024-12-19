c752c3bb0cab5bdcd8f795a16a2c6b225864ee9b
saygo-png <saygo.mail@proton.me>
2024-12-19 23:09:54 +0100

    feat: remove deadnix
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- treefmt.nix
+++ treefmt.nix
@@ -3,8 +3,7 @@ _: {
 
   programs.alejandra.enable = true;
 
-  # Deadnix is used as a github action
-  # programs.deadnix.enable = true;
+  programs.deadnix.enable = true;
 
   programs.statix.enable = true;
   settings.global.excludes = ["resources/cookiecutterTemplates/*"];
