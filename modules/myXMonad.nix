b4ea662896857a3a626f5e004813d26224befaaf
saygo-png <saygo.mail@proton.me>
2024-12-19 13:52:47 +0100

    feat: early xmonad config
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- modules/myXMonad.nix
+++ modules/myXMonad.nix
@@ -1,4 +1,5 @@
 {
+  pkgs,
   lib,
   conUsername,
   conFlakePathRel,
@@ -8,27 +9,43 @@
     "${conFlakePathRel}/modules/x11/myXorgBase.nix"
   ];
 
-  services.displayManager.defaultSession = lib.mkDefault "none+xmonad";
+  # services.xserver.windowManager.xmonad = {
+  #   enable = true;
+  #   enableContribAndExtras = true;
+  #   # flake = {
+  #   #   enable = true;
+  #   #   compiler = "ghc947";
+  #   # };
+  #   config = builtins.readFile "${conFlakePathRel}/resources/xmonad/config.hs";
+  #   enableConfiguredRecompile = false;
+  # };
 
-  services.xserver.windowManager.xmonad = {
-    enable = true;
-    enableContribAndExtras = true;
-    # flake = {
-    #   enable = true;
-    #   compiler = "ghc947";
-    # };
-    # config = builtins.readFile ../xmonad/xmonad.hs;
-    enableConfiguredRecompile = false;
-  };
+  services.displayManager.defaultSession = "none+xmonad";
 
   home-manager.users.${conUsername} = {
     # osConfig,
     # config,
     ...
   }: {
+
+    home.packages = with pkgs; [
+      xmobar
+    ];
+
+    xdg.configFile."xmonad" = {
+      source = "${conFlakePathRel}/resources/xmonad";
+      recursive = true;
+    };
+
     xsession.windowManager.xmonad = {
       enableContribAndExtras = true;
       enable = true;
+      extraPackages = hpkgs:
+        with hpkgs; [
+          xmonad
+          xmonad-contrib
+          xmonad-extras
+        ];
     };
   };
 }
