1a9eeabda6a375a2fb1ecff542f844a4ac9f2214
saygo-png <saygo.mail@proton.me>
2024-12-24 00:56:53 +0100

    feat: add gui Mullvad client
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- modules/myMullvad.nix
+++ modules/myMullvad.nix
@@ -1,13 +1,14 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  conUsername,
+  ...
+}: {
   services.mullvad-vpn.enable = true;
   networking.firewall.checkReversePath = lib.mkForce "strict";
-  # environment.systemPackages = with pkgs; [
-  #   mullvad-client
-  # ];
-
-  # home-manager.users.${conUsername} = {config, ...}: {
-  #   home.packages = with pkgs; [
-  #     mullvad-client
-  #   ];
-  # };
+  home-manager.users.${conUsername} = {
+    home.packages = with pkgs; [
+      mullvad-vpn
+    ];
+  };
 }
