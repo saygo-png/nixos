fdf694b58f0b211ee49752e2080ec8cdb942b814
saygo-png <saygo.mail@proton.me>
2024-12-28 11:45:24 +0100

    fix: open at last location error in neovim
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- modules/myNeovim.nix
+++ modules/myNeovim.nix
@@ -333,6 +333,26 @@ in {
           vim.cmd [[ hi Normal guibg=${config.lib.stylix.colors.withHashtag.base00} ]]
         end
 
+        vim.api.nvim_create_autocmd("BufRead", {
+          callback = function(opts)
+            vim.api.nvim_create_autocmd("BufWinEnter", {
+              once = true,
+              buffer = opts.buf,
+              callback = function()
+                local ft = vim.bo[opts.buf].filetype
+                local last_known_line = vim.api.nvim_buf_get_mark(opts.buf, '"')[1]
+                if
+                  not (ft:match("commit") and ft:match("rebase"))
+                  and last_known_line > 1
+                  and last_known_line <= vim.api.nvim_buf_line_count(opts.buf)
+                then
+                  vim.api.nvim_feedkeys([[g`"]], "nx", false)
+                end
+              end,
+            })
+          end,
+        })
+
         -- Statusline {{{
         -- Statusline components
         local cmp = {}
@@ -1399,12 +1419,6 @@ in {
 
       # }}}
       autoCmd = [
-        {
-          event = ["BufReadPost"];
-          pattern = ["*"];
-          command = "normal!'\"";
-          desc = "Open at last location";
-        }
         {
           event = ["BufEnter"];
           pattern = ["*"];
