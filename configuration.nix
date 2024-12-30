b62bb4f8f60b41532411bd77235d882d50f61d7d
saygo-png <saygo.mail@proton.me>
2024-12-30 16:47:35 +0100

    feat: dont pull in entire plasma workspaces to make dolphin work
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- configuration.nix
+++ configuration.nix
@@ -243,7 +243,21 @@
   programs.gphoto2.enable = true;
 
   # Fixes dolphin not having mime types.
-  environment.etc."/xdg/menus/applications.menu".text = builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+  environment.etc."/xdg/menus/applications.menu".text =
+    # This one also works but has to pull in entire plasma workspaces, but might be more future proof
+    # builtins.readFile "${pkgs.kdePackages.plasma-workspace}/etc/xdg/menus/plasma-applications.menu";
+    ''
+      <!DOCTYPE Menu PUBLIC "-//freedesktop//DTD Menu 1.0//EN"
+       "http://www.freedesktop.org/standards/menu-spec/1.0/menu.dtd">
+
+      <Menu>
+          <Name>Applications</Name>
+
+          <DefaultAppDirs/>
+          <DefaultDirectoryDirs/>
+          <DefaultMergeDirs/>
+      </Menu>
+    '';
 
   # System packages.
   environment.systemPackages = with pkgs;
