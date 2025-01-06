74113ac3c79f59df6b9db2fbe1a05db93d05754b
saygo-png <saygo.mail@proton.me>
2025-01-06 16:09:02 +0100

    feat: increase border width in xmonad
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- resources/haskell/xmonad/lib/Defaults.hs
+++ resources/haskell/xmonad/lib/Defaults.hs
@@ -19,7 +19,7 @@ myClickJustFocuses = False
 
 -- Width of the window border in pixels.
 myBorderWidth :: Dimension
-myBorderWidth = 2
+myBorderWidth = 3
 
 -- modMask lets you specify which modkey you want to use. The default
 -- is mod1Mask ("left alt").  You may also consider using mod3Mask
