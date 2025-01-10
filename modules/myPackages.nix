0df3da04d1e92db9db1922098138a48410b14ee4
saygo-png <saygo.mail@proton.me>
2025-01-10 03:07:27 +0100

    feat: make connection tester use "watch" util
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- modules/myPackages.nix
+++ modules/myPackages.nix
@@ -229,47 +229,57 @@
           rm -f ${conHome}/.local/state/nix/profiles/home-manager*
         '')
 
-      (writeShellScriptBin "connection-tester"
-        ''
-          connected_to_internet() {
-            test_urls="\
-            https://www.google.com/ \
-            https://www.duck.com/ \
-            https://www.cloudflare.com/ \
-            "
-            processes="0"
-            pids=""
-            for test_url in $test_urls; do
-              curl --silent --head "$test_url" > /dev/null &
-              pids="$pids $!"
-              processes=$(($processes + 1))
-            done
-            while [ $processes -gt 0 ]; do
-              for pid in $pids; do
-                if ! ps | grep "^[[:blank:]]*$pid[[:blank:]]" > /dev/null; then
-                  # Process no longer running
-                  processes=$(($processes - 1))
-                  pids=$(echo "$pids" | sed --regexp-extended "s/(^| )$pid($| )/ /g")
-
-                  if wait $pid; then
-                    # Success! We have a connection to at least one public site, so the
-                    # internet is up. Ignore other exit statuses.
-                    kill -TERM $pids > /dev/null 2>&1 || true
-                    wait $pids
-                    return 0
-                  fi
-                fi
+      (let
+        connection-test =
+          writeShellScriptBin "connection-test"
+          ''
+            connected_to_internet() {
+              test_urls="\
+              https://www.google.com/ \
+              https://www.duck.com/ \
+              https://www.cloudflare.com/ \
+              "
+              processes="0"
+              pids=""
+              for test_url in $test_urls; do
+                curl --silent --head "$test_url" > /dev/null &
+                pids="$pids $!"
+                processes=$(($processes + 1))
               done
-              wait -n $pids # Better than sleep, but not supported on all systems
-            done
-            return 1
-          }
-          if connected_to_internet; then
-              echo "Connected to internet" && ${lib.getExe pkgs.libnotify} "Connected to internet"
-            else
-              echo "No internet connection"
-          fi
-        '')
+              while [ $processes -gt 0 ]; do
+                for pid in $pids; do
+                  if ! ps | grep "^[[:blank:]]*$pid[[:blank:]]" > /dev/null; then
+                    # Process no longer running
+                    processes=$(($processes - 1))
+                    pids=$(echo "$pids" | sed --regexp-extended "s/(^| )$pid($| )/ /g")
+
+                    if wait $pid; then
+                      # Success! We have a connection to at least one public site, so the
+                      # internet is up. Ignore other exit statuses.
+                      kill -TERM $pids > /dev/null 2>&1 || true
+                      wait $pids
+                      return 0
+                    fi
+                  fi
+                done
+                wait -n $pids # Better than sleep, but not supported on all systems
+              done
+              return 1
+            }
+            if connected_to_internet; then
+                echo "Connected to internet" && ${lib.getExe pkgs.libnotify} "Connected to internet"
+              else
+                echo "No internet connection"
+            fi
+          '';
+      in
+        writeShellApplication {
+          name = "connection-tester";
+          runtimeInputs = with pkgs; [coreutils];
+          text = ''
+            watch -n 0.5 ${lib.getExe connection-test}
+          '';
+        })
 
       (writeShellApplication {
         name = "mynix-list-packages";
