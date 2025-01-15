f244fb6f9a357f2984670b43bf78cd9fba1e9a2a
saygo-png <saygo.mail@proton.me>
2025-01-15 01:21:10 +0100

    feat: less harsh battery optimization
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- hosts/thinkpad/thinkpad.nix
+++ hosts/thinkpad/thinkpad.nix
@@ -111,13 +111,13 @@
       # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
       CPU_MIN_PERF_ON_BAT = 0;
       CPU_MAX_PERF_ON_BAT = 50;
 
       PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "low-power";
+      PLATFORM_PROFILE_ON_BAT = "balanced";
 
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
