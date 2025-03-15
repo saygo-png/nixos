4dbc4c1743e9379d6633774ba4bb7547e992c1d1
saygo-png <saygo.mail@proton.me>
2025-03-15 17:32:30 +0100

    feat: rmoeve empty lines in treefmt
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- treefmt.nix
+++ treefmt.nix
@@ -1,4 +1,8 @@
-_: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   projectRootFile = "flake.nix";
 
   programs.alejandra.enable = true;
@@ -7,4 +11,11 @@ _: {
 
   programs.statix.enable = true;
   settings.global.excludes = ["resources/cookiecutterTemplates/*"];
+  settings.formatter = {
+    trim-newlines = {
+      command = "${lib.getExe pkgs.gnused}";
+      options = lib.strings.splitString " " ''-i -e :a -e /^\n*$/{$d;N;ba -e }'';
+      includes = ["*"];
+    };
+  };
 }
