a47d761ee989553bef495688bff591108aff88d4
saygo-png <saygo.mail@proton.me>
2025-03-15 17:32:52 +0100

    chore: removed unused imports
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- modules/myAudioEffects.nix
+++ modules/myAudioEffects.nix
@@ -1,8 +1,5 @@
 {
   pkgs,
-  inputs,
-  conUsername,
-  conFlakePathRel,
   lib,
   ...
 }: {
--- modules/myXMonad.nix
+++ modules/myXMonad.nix
@@ -4,7 +4,6 @@
   extraLib,
   conUsername,
   conFlakePathRel,
-  conFlakePath,
   ...
 }: {
   imports = [
