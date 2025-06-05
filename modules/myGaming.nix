b9736d283e1899025c22d39552edc4bfba2376fa
saygo-png <131687037+saygo-png@users.noreply.github.com>
2025-06-05 16:13:26 +0000

    chore: run auto formatting and linting tools

--- modules/myGaming.nix
+++ modules/myGaming.nix
@@ -63,7 +63,7 @@
     (
       pkgs.writeShellScriptBin
       "sgamescope" # [s]team [gamescope]
-
+      
       ''
         gamescope \
           -w ${builtins.toString config.const.screenWidth} \
