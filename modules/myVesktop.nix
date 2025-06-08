cf73e9d71eaa25256b625dc9b73a54100a63f189
saygo-png <saygo.mail@proton.me>
2025-06-08 11:08:04 +0200

    feat: vesktop better patch
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- modules/myVesktop.nix
+++ modules/myVesktop.nix
@@ -1,28 +1,60 @@
 {
   lib,
   pkgs,
+  config,
   ...
 }: {
   nixpkgs.overlays = [
     (_final: prev: {
       vesktop = prev.vesktop.overrideAttrs (oldAttrs: {
-        preConfigurePhases = oldAttrs.preConfigurePhases or [] ++ ["patchImages"];
+        preConfigurePhases = (oldAttrs.preConfigurePhases or []) ++ ["myPatchImages"];
 
-        patchImages = ''
+        myPatchImages = let
+          patchFile = builtins.toFile "patch.txt" ''
+            46,51c46
+            <         <img
+            <             draggable="false"
+            <             src="../shiggy.gif"
+            <             alt="shiggy"
+            <             role="presentation"
+            <         />
+            ---
+            >         <span class="loader"></span>
+          '';
+          spinnerCss =
+            # css
+            let
+              inherit (config.lib.stylix.colors) withHashtag;
+            in ''
+              .loader {
+                width: 48px;
+                height: 48px;
+                border: 5px solid ${withHashtag.base06};
+                border-bottom-color: transparent;
+                border-radius: 50%;
+                display: inline-block;
+                box-sizing: border-box;
+                animation: rotation 1s linear infinite;
+              }
+
+              @keyframes rotation {
+                0% {
+                    transform: rotate(0deg);
+                }
+                100% {
+                    transform: rotate(360deg);
+                }
+              }
+            '';
+        in ''
           rm static/shiggy.gif
-
-          substituteInPlace static/views/splash.html \
-            --replace-fail shiggy.gif loading.gif
-
-          install -m0644 ${lib.my.relativeToRoot "resources/vesktop/loading.gif"} static/
+          echo ${lib.strings.escapeShellArg spinnerCss} >> static/views/style.css
+          cat static/views/style.css
+          patch static/views/splash.html ${patchFile}
         '';
       });
     })
   ];
 
-  custom.allowedUnfreePkgs = [pkgs.vesktop];
-
-  environment.systemPackages = [
-    pkgs.vesktop
-  ];
+  environment.systemPackages = [pkgs.vesktop];
 }
