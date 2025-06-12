59e22b29880761c1b67936cb2a58c3801f3aa917
saygo-png <saygo.mail@proton.me>
2025-06-12 13:50:29 +0200

    feat: install script
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- install.bash
+++ install.bash
@@ -1,8 +1,20 @@
 #!/usr/bin/env bash
 # On the install iso, put a password file made with `mkpasswd` in /etc/password.txt
 # Run this from inside my dotfiles directory root since path to flake is relative.
+# Put the disk path like /dev/nvme0n1 into the variable below
+
+DISK=UNDEFINED
+
+[ "$DISK" = "UNDEFINED" ] && exit
+
+PASSWORD_FILE=/etc/password.txt
+
+wipefs -a "$DISK"
+blkdiscard -f "$DISK"
+sgdisk --zap-all "$DISK"
+zpool labelclear -f "$DISK"
 
 sudo nix run 'github:nix-community/disko/latest#disko-install' \
   --extra-experimental-features "nix-command flakes" -- \
-  --flake .#install --disk main DISKDEVICEHERE \
-  --extra-files /etc/password.txt /etc/password.txt
+  --flake .#install --disk main "$DISK" \
+  --extra-files "$PASSWORD_FILE" /etc/password.txt
