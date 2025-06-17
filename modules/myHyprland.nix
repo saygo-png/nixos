c36017bcf920671497a72c773085d8c2b1b4d931
saygo-png <saygo.mail@proton.me>
2025-06-18 01:48:51 +0200

    feat(hyprland): enable autoreload
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- modules/myHyprland.nix
+++ modules/myHyprland.nix
@@ -338,7 +338,7 @@ in {
         misc = {
           enable_anr_dialog = false;
           enable_swallow = false;
-          disable_autoreload = true;
+          disable_autoreload = false;
           # Hides text on bottom of the screen.
           disable_hyprland_logo = true;
           animate_manual_resizes = true;
