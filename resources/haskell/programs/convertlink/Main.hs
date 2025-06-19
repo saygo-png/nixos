f272e85a5be79db0e5e93319a57d7571992363f6
saygo-png <saygo.mail@proton.me>
2025-06-19 09:51:20 +0200

    feat: convertlink update
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- resources/haskell/programs/convertlink/Main.hs
+++ resources/haskell/programs/convertlink/Main.hs
@@ -2,13 +2,15 @@
 {-# LANGUAGE BangPatterns, BinaryLiterals, ConstraintKinds, DataKinds, DefaultSignatures, DeriveDataTypeable, DeriveFoldable, DeriveFunctor, DeriveGeneric, DeriveTraversable, DoAndIfThenElse, EmptyDataDecls, ExistentialQuantification, FlexibleContexts, FlexibleInstances, FunctionalDependencies, GADTs, GeneralizedNewtypeDeriving, InstanceSigs, KindSignatures, LambdaCase, MultiParamTypeClasses, MultiWayIf, NamedFieldPuns, NoImplicitPrelude, OverloadedStrings, PartialTypeSignatures, PatternGuards, PolyKinds, RankNTypes, RecordWildCards, ScopedTypeVariables, StandaloneDeriving, TupleSections, TypeFamilies, TypeSynonymInstances, ViewPatterns #-}
 {- ORMOLU_ENABLE -}
 
+import Flow
 import Options.Applicative
 import RIO
 import System.Directory
+import System.FilePath
 import System.Posix.Files
 import Text.Printf (printf)
 
-data Options = Options {path :: FilePath, write :: Bool}
+data Options = Options {path :: FilePath, write :: Bool, recurse :: Bool}
 
 optionsParser :: Parser Options
 optionsParser =
@@ -24,6 +26,11 @@ optionsParser =
           <> short 'w'
           <> help "Give owner write permissions to the resulting file"
       )
+    <*> switch
+      ( long "recursive"
+          <> short 'r'
+          <> help "Convert symlinks in a directory recursively"
+      )
 
 parserInfo :: ParserInfo Options
 parserInfo = info (helper <*> optionsParser) (progDesc "Convert a symlink to the file its pointing to")
@@ -34,27 +41,35 @@ main = convertlinksAndWrite =<< execParser parserInfo
 convertlinksAndWrite :: Options -> IO ()
 convertlinksAndWrite opts = do
   let filepath = path opts
-  let giveWrite = write opts
+      giveWrite = write opts
+      recursive = recurse opts
   filestatus <- getFileStatus filepath
 
-  if isDirectory filestatus
-    then printf "Directories are not supported\n"
-    else
-      pathIsSymbolicLink filepath >>= \case
-        True -> convertLinkAndWrite filepath giveWrite
-        False -> printf "convertlink: cannot convert %s: Not a symlink\n" filepath
+  case isDirectory filestatus of
+    True
+      | not recursive -> printf "convertlink: -r not specified; omitting directory %s\n" filepath
+      | otherwise -> do
+          symlinks <- findSymlinksRecursively filepath
+          if symlinks /= []
+            then mapM_ (`convertLinkAndWrite` giveWrite) symlinks
+            else printf "convertlink: directory doesn't contain symlinks %s\n" filepath
+    False -> do
+      isSymlink <- pathIsSymbolicLink filepath
+      if isSymlink
+        then convertLinkAndWrite filepath giveWrite
+        else printf "convertlink: cannot convert %s: Not a symlink\n" filepath
 
 convertLinkAndWrite :: FilePath -> Bool -> IO ()
 convertLinkAndWrite filepath write = do
   convertSymlinkPreservePerms filepath
   printf "Converted symlink %s\n" filepath
-  when write $ do
+  when write <| do
     addOwnerWritePermission filepath
 
 addOwnerWritePermission :: FilePath -> IO ()
 addOwnerWritePermission filepath = do
   currentMode <- fileMode <$> getFileStatus filepath
-  setFileMode filepath (currentMode `unionFileModes` ownerWriteMode)
+  currentMode `unionFileModes` ownerWriteMode |> setFileMode filepath
 
 convertSymlinkPreservePerms :: FilePath -> IO ()
 convertSymlinkPreservePerms symlink = do
@@ -64,3 +79,24 @@ convertSymlinkPreservePerms symlink = do
   removeLink symlink
   copyFile fileFromSymlink symlink
   setFileMode symlink targetMode
+
+findSymlinksRecursively :: FilePath -> IO [FilePath]
+findSymlinksRecursively dir = do
+  result <- try <| listDirectory dir
+  case result of
+    Left (_ :: SomeException) -> return []
+    Right entries -> do
+      let fullPaths = map (dir </>) entries
+      symlinkResults <- mapM checkAndRecurse fullPaths
+      return <| concat symlinkResults
+  where
+    checkAndRecurse :: FilePath -> IO [FilePath]
+    checkAndRecurse path = do
+      isSymlink <- pathIsSymbolicLink path
+      if isSymlink
+        then return [path]
+        else do
+          isDir <- doesDirectoryExist path
+          if isDir
+            then findSymlinksRecursively path
+            else return []
