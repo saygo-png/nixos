8b14530fa3512e45735f4ac31637485ef65bd860
saygo-png <saygo.mail@proton.me>
2025-06-24 09:40:30 +0200

    feat: init reltime
    Signed-off-by: saygo-png <saygo.mail@proton.me>

---
+++ resources/haskell/reltime/default.nix
@@ -0,0 +1,14 @@
+{pkgs}: let
+  pkg = pkgs.haskellPackages.developPackage {
+    root =
+      pkgs.nix-gitignore.gitignoreSourcePure
+      [
+        "dist-newstyle"
+        ".*#"
+        ".git"
+      ]
+      ./.;
+    returnShellEnv = false;
+  };
+in
+  pkgs.haskellPackages.generateOptparseApplicativeCompletions ["reltime"] pkg
