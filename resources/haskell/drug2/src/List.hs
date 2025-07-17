8e098301177da17d74dd367e68da73b00e7a85b2
saygo-png <saygo.mail@proton.me>
2025-07-18 01:20:32 +0200

    feat(drug2): align using my own pretty printer, remove dependency
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- resources/haskell/drug2/src/List.hs
+++ resources/haskell/drug2/src/List.hs
@@ -9,7 +9,6 @@ import Data.Csv qualified as Cassava
 import Data.Function ((&))
 import Data.Text qualified as T
 import Data.Vector qualified as V
-import Text.Layout.Table
 import Text.Time.Pretty
 import Types
 
@@ -27,14 +26,32 @@ listDrugs = do
 takeLast :: (IsSequence seq) => Index seq -> seq -> seq
 takeLast i l = reverse l & take i & reverse
 
+type Row = Vector Text
+
+type Table = Vector Row
+
+padRToLen :: Int -> Text -> Text
+padRToLen maxLen t = t <> replicate (maxLen - length t) ' '
+
+prettyTable :: Table -> Text
+prettyTable t =
+  let cols = V.maximum $ length <$> t
+
+      columnWidths = V.generate cols $
+        \col -> V.maximum $ V.mapMaybe (\row -> length <$> (row V.!? col)) t
+
+      formatRow row =
+        intercalate " | "
+          . toList
+          $ V.imap (\col cell -> padRToLen (columnWidths V.! col) cell) row
+  in  intercalate "\n" $ formatRow <$> t
+
 prettyPrint :: Vector DrugLine -> IO ()
 prettyPrint vec = do
   nl <- niceLines vec
-  let few = takeLast 14 nl
-  let table = gridString [def, def, def] (toList (fmap V.toList few))
-  putStrLn $ T.pack table
+  putStrLn . prettyTable $ takeLast 14 nl
 
-niceLines :: Vector DrugLine -> IO (Vector (Vector Text))
+niceLines :: Vector DrugLine -> IO Table
 niceLines vec = do
   let nums = V.generate (length vec) (\x -> tshow $ x + 1)
   let names = fromMaybe "DRUG NAME MISSING" . drugData <$> vec
