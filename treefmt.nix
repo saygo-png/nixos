dda6e4cfe006caacf67e2052a1d80a2b6384ea39
saygo-png <saygo.mail@proton.me>
2025-07-18 15:55:43 +0200

    feat: haskell formatting with treefmt
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- treefmt.nix
+++ treefmt.nix
@@ -6,12 +6,24 @@
   projectRootFile = "flake.nix";
 
   programs.alejandra.enable = true;
-
   programs.deadnix.enable = true;
-
+  programs.fourmolu.enable = true;
   programs.statix.enable = true;
-  settings.global.excludes = ["resources/cookiecutterTemplates/*"];
+
   settings.formatter = {
+    fourmolu = {
+      options = [
+        "--indentation"
+        "2"
+
+        "--respectful"
+        "false"
+
+        "--indent-wheres"
+        "true"
+      ];
+    };
+
     trim-newlines = {
       command = "${lib.getExe pkgs.gnused}";
       options = lib.strings.splitString " " ''-i -e :a -e /^\n*$/{$d;N;ba -e }'';
