a25c778f1da15f2a4d38cfe3e9edd265c3db68b5
saygo-png <saygo.mail@proton.me>
2025-08-01 09:07:23 +0200

    feat: add iamb
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- configuration.nix
+++ configuration.nix
@@ -617,6 +617,34 @@
       programs.home-manager.enable = true;
       programs.git-credential-oauth.enable = true;
 
+      programs.iamb = {
+        enable = true;
+        settings = {
+          default_profile = "saygo";
+          profiles.saygo.user_id = "@saygo.2:tchncs.de";
+          layout.style = "restore";
+          settings = {
+            message_user_color = true;
+            username_display = "localpart";
+            user_gutter_width = 9;
+            notifications = {
+              enabled = true;
+              via = "desktop";
+              show_message = true;
+            };
+          };
+          macros.normal = {
+            ";" = ":";
+            u = "\"+p";
+            s = "<C-w>m";
+            gx = ":open l<Enter>";
+            mr = ":reply<Enter>";
+            mq = ":cancel<Enter>y";
+            mv = ":editor<Enter>";
+          };
+        };
+      };
+
       stylix.targets.zathura.enable = false;
       programs.zathura = let
         color = config.lib.stylix.colors.withHashtag;
