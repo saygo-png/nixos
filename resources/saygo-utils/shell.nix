efd221913ac9a480953fa3deca95ea9e4554045c
saygo-png <saygo.mail@proton.me>
2025-12-14 08:27:18 +0100

    fix: saygo-utils shell
    Signed-off-by: saygo-png <saygo.mail@proton.me>

--- resources/saygo-utils/shell.nix
+++ resources/saygo-utils/shell.nix
@@ -1,9 +1,20 @@
-{pkgs ? import <nixpkgs> {}}:
+{
+  pkgs ?
+    import (
+      fetchTree {
+        type = "github";
+        owner = "NixOS";
+        repo = "nixpkgs";
+        rev = "f4b140d5b253f5e2a1ff4e5506edbf8267724bde";
+      }
+    ) {},
+}:
 pkgs.mkShell {
   packages = let
     ghcPackages = pkgs.haskell.packages.ghc984;
   in [
     ghcPackages.haskell-language-server
     ghcPackages.ghc
+    ghcPackages.cabal-install
   ];
 }
